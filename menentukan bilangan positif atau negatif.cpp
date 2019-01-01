611661f57deb35b43cebc4d5276fb391f026a92b
mukromun 311810151 <45851430+mukromun311810151@users.noreply.github.com>
2019-01-01 21:43:05 +0700

    Add files via upload

---
+++ menentukan bilangan positif atau negatif.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int a;
+	cout <<"========================================"<<endl;
+	cout <<"MENENTUKAN BILANGAN NEGATIF ATAU POSITIF"<<endl;
+	cout <<"========================================"<<endl;
+	cout <<"MASUKKAN BILANGAN : ";
+	cin >>a;
+	
+	if (a>0)
+	cout <<"BILANGAN TERSEBUT BERNILAI POSITIF"<<endl;
+	else
+	cout <<"BILANGAN TERSEBUT BERNILAI NEGATIF"<<endl;
+	
+	return 0;
+}
